14c1ea9bfc2b5a12757ec28693c992b4b45f926f
Ivano Bilenchi <ivanobilenchi@gmail.com>
2023-01-17 14:37:46 +0100

    Use 'ustring_wrap_buf' where appropriate

--- test/tests/utime_tests.c
+++ test/tests/utime_tests.c
@@ -36,7 +36,7 @@ bool utime_test_ns(void) {
         utest_assert_uint(utime_interval_unit_auto(data.t), ==, data.unit);
 
         UString str = utime_interval_to_string(data.t, data.unit);
-        utest_assert_ustring(str, ==, ustring_wrap(data.str, strlen(data.str)));
+        utest_assert_ustring(str, ==, ustring_wrap_buf(data.str));
         ustring_deinit(&str);
     }
 
